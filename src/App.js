2a9681ed2d278f40012f311a589a5311b91a5b55
Joao Gomes <joao.gomes@ifactory.com.br>
2019-05-23 11:35:16 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import NewComponent from './NewComponent.js';
+
+class App extends Component {
+  render() {    
+    return (
+      <NewComponent></NewComponent>
+    );
+  }
+}
+
+export default App;
