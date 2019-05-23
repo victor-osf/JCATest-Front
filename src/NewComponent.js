a21f83b40531fb714822e5c0661a0745d6d0acd3
Joao Gomes <joao.gomes@ifactory.com.br>
2019-05-23 12:04:17 -0300

    fix link issue

--- src/NewComponent.js
+++ src/NewComponent.js
@@ -200,9 +200,9 @@ const NewComponent = () =>(
           <footer className="my-5 pt-5 text-muted text-center text-small">
             <p className="mb-1">© 2017-2018 Company Name</p>
             <ul className="list-inline">
-              <li className="list-inline-item"><a href="#">Privacy</a></li>
-              <li className="list-inline-item"><a href="#">Terms</a></li>
-              <li className="list-inline-item"><a href="#">Support</a></li>
+              <li className="list-inline-item"><a href="#top">Privacy</a></li>
+              <li className="list-inline-item"><a href="#top">Terms</a></li>
+              <li className="list-inline-item"><a href="#top">Support</a></li>
             </ul>
           </footer>
         </div>      
