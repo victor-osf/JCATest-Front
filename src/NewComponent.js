1672e5a6a943fea4090ada3467a0147d7505d851
Joao Gomes <joao.gomes@ifactory.com.br>
2019-05-27 09:50:02 -0300

    defaultValue

--- src/NewComponent.js
+++ src/NewComponent.js
@@ -23,7 +23,7 @@ const NewComponent = () =>(
                         <input id="cometa" name="baseUrl" type="radio" className="custom-control-input" defaultChecked  />
                         <label className="custom-control-label" htmlFor="cometa">Cometa</label>
                       </h6>
-                        <small class="text-muted">http://jca.ifactory.com.br/content/jca/cometa/en.html</small>
+                        <small className="text-muted list-group">http://jca.ifactory.com.br/content/jca/cometa/en.html</small>
                     </div>
                 </li>
                 <li className="list-group-item d-flex justify-content-between lh-condensed">
@@ -32,7 +32,7 @@ const NewComponent = () =>(
                         <input id="rapido" name="baseUrl" type="radio" className="custom-control-input"  />
                         <label className="custom-control-label" htmlFor="rapido">Rápido Ribeirão</label>
                       </h6>
-                      <small class="text-muted">http://jca.ifactory.com.br/content/jca/rapido-ribeirao/en.html</small>
+                      <small className="text-muted">http://jca.ifactory.com.br/content/jca/rapido-ribeirao/en.html</small>
                     </div>
                 </li>
                 <li className="list-group-item d-flex justify-content-between lh-condensed">
@@ -41,7 +41,7 @@ const NewComponent = () =>(
                         <input id="catarinense" name="baseUrl" type="radio" className="custom-control-input"  />
                         <label className="custom-control-label" htmlFor="catarinense">Catarinense</label>
                       </h6>
-                      <small class="text-muted">http://jca.ifactory.com.br/content/jca/catarinense/en.html</small>
+                      <small className="text-muted list-group">http://jca.ifactory.com.br/content/jca/catarinense/en.html</small>
                     </div>
                 </li>
                 <li className="list-group-item d-flex justify-content-between lh-condensed">
@@ -50,7 +50,7 @@ const NewComponent = () =>(
                         <input id="1001" name="baseUrl" type="radio" className="custom-control-input"  />
                         <label className="custom-control-label" htmlFor="1001">1001</label>
                       </h6>
-                      <small class="text-muted">http://jca.ifactory.com.br/content/jca/viacao1001/en.html</small>
+                      <small className="text-muted">http://jca.ifactory.com.br/content/jca/viacao1001/en.html</small>
                     </div>
                 </li>
                 <li className="list-group-item d-flex justify-content-between lh-condensed">
@@ -59,7 +59,7 @@ const NewComponent = () =>(
                         <input id="expresso" name="baseUrl" type="radio" className="custom-control-input"  />
                         <label className="custom-control-label" htmlFor="expresso">Expresso do Sul</label>
                       </h6>
-                      <small class="text-muted">http://jca.ifactory.com.br/content/jca/expresso-sul/en.html</small>
+                      <small className="text-muted">http://jca.ifactory.com.br/content/jca/expresso-sul/en.html</small>
                     </div>
                 </li>
                 
@@ -84,31 +84,31 @@ const NewComponent = () =>(
                 <div className="row">
                   <div className="col-md-6 mb-3">
                     <label htmlFor="userCPF">CPF do usuário</label>
-                    <input type="text" className="form-control" id="userCPF"  />                    
+                    <input type="text" className="form-control" id="userCPF" defaultValue="270.220.058-34" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="passCPF">Senha login CPF</label>
-                    <input type="text" className="form-control" id="passCPF"  />                    
+                    <input type="text" className="form-control" id="passCPF" defaultValue="123456" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="userCNPJ">CNPJ do usuário</label>
-                    <input type="text" className="form-control" id="userCNPJ"  />                    
+                    <input type="text" className="form-control" id="userCNPJ" defaultValue="03.884.716/0001-97" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="passCNPJ">Senha login CNPJ</label>
-                    <input type="text" className="form-control" id="passCNPJ"  />                    
+                    <input type="text" className="form-control" id="passCNPJ" defaultValue="/?-+={}[]#(" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="userPassport">Passaporte do usuário</label>
-                    <input type="text" className="form-control" id="userPassport"  />                    
+                    <input type="text" className="form-control" id="userPassport" defaultValue="PAS123" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="passPassport">Senha login Passaporte</label>
-                    <input type="text" className="form-control" id="passPassport"  />                    
+                    <input type="text" className="form-control" id="passPassport" defaultValue="123456" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="userCPFWrong">CPF inválido</label>
-                    <input type="text" className="form-control" id="userCPFWrong"  />                    
+                    <input type="text" className="form-control" id="userCPFWrong" defaultValue="122.222.222-88" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                   <label htmlFor="username">Email de login do usuário</label>
@@ -116,7 +116,7 @@ const NewComponent = () =>(
                       <div className="input-group-prepend">
                         <span className="input-group-text">@</span>
                       </div>
-                      <input type="text" className="form-control" id="username"  />                    
+                      <input type="text" className="form-control" id="username" defaultValue="jullianamartins@gmail.com" />                    
                     </div>                   
                   </div> 
                 </div>
@@ -125,60 +125,60 @@ const NewComponent = () =>(
                 <div className="row">
                   <div className="col-md-6 mb-3">
                     <label htmlFor="passenger1">Passageiro 1</label>
-                    <input type="text" className="form-control" id="passenger1"  />                    
+                    <input type="text" className="form-control" id="passenger1" defaultValue="Francisco Anthony Mendes" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="docPassenger1">Documento do passageiro 1</label>
-                    <input type="text" className="form-control" id="docPassenger1"  />                    
+                    <input type="text" className="form-control" id="docPassenger1" defaultValue="359.245.261-43" />                    
                   </div>
 
                   <div className="col-md-6 mb-3">
                     <label htmlFor="passenger2">Passageiro 2</label>
-                    <input type="text" className="form-control" id="passenger2"  />                    
+                    <input type="text" className="form-control" id="passenger2" defaultValue="Theo Cauê Moraes" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="docPassenger2">Documento do passageiro 2</label>
-                    <input type="text" className="form-control" id="docPassenger2"  />                    
+                    <input type="text" className="form-control" id="docPassenger2" defaultValue="16.703.153-3" />                    
                   </div>
 
                   <div className="col-md-6 mb-3">
                     <label htmlFor="passenger3">Passageiro 3</label>
-                    <input type="text" className="form-control" id="passenger3"  />                    
+                    <input type="text" className="form-control" id="passenger3" defaultValue="Daniela Nina Santos" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="docPassenger3">Documento do passageiro 3</label>
-                    <input type="text" className="form-control" id="docPassenger3"  />                    
+                    <input type="text" className="form-control" id="docPassenger3" defaultValue="30.988.353-2" />                    
                   </div>
 
                   <div className="col-md-6 mb-3">
                     <label htmlFor="passenger4">Passageiro 4</label>
-                    <input type="text" className="form-control" id="passenger4"  />                    
+                    <input type="text" className="form-control" id="passenger4" defaultValue="Caroline Agatha Yasmin Teixeira" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="docPassenger4">Documento do passageiro 4</label>
-                    <input type="text" className="form-control" id="docPassenger4"  />                    
+                    <input type="text" className="form-control" id="docPassenger4" defaultValue="024.692.438-15" />                    
                   </div>
 
                   <div className="col-md-6 mb-3">
                     <label htmlFor="passenger5">Passageiro 5</label>
-                    <input type="text" className="form-control" id="passenger5"  />                    
+                    <input type="text" className="form-control" id="passenger5" defaultValue="Márcia Beatriz Maya Porto" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="docPassenger5">Documento do passageiro 5</label>
-                    <input type="text" className="form-control" id="docPassenger5"  />                    
+                    <input type="text" className="form-control" id="docPassenger5" defaultValue="240.556.753-99" />                    
                   </div>
 
                   <div className="col-md-6 mb-3">
                     <label htmlFor="passenger6">Passageiro 6</label>
-                    <input type="text" className="form-control" id="passenger6"  />                    
+                    <input type="text" className="form-control" id="passenger6" defaultValue="Cauê Sérgio Pires" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="docPassenger6">Documento do passageiro 6</label>
-                    <input type="text" className="form-control" id="docPassenger6"  />                    
+                    <input type="text" className="form-control" id="docPassenger6" defaultValue="PAS123456" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="studentCpf">CPF do estudante</label>
-                    <input type="text" className="form-control" id="studentCpf"  />                    
+                    <input type="text" className="form-control" id="studentCpf" defaultValue="34624470842" />                    
                   </div>
                 </div>
 
@@ -186,15 +186,15 @@ const NewComponent = () =>(
                 <div className="row">
                   <div className="col-md-6 mb-3">
                     <label htmlFor="numCard">Número do cartão de crédito</label>
-                    <input type="text" className="form-control" id="numCard"  />                    
+                    <input type="text" className="form-control" id="numCard" defaultValue="4111 1111 1111 1111" />                    
                   </div>
                   <div className="col-md-3 mb-3">
                     <label htmlFor="valDate">Data de validade</label>
-                    <input type="text" className="form-control" id="valDate"  />                    
+                    <input type="text" className="form-control" id="valDate" defaultValue="10/20" />                    
                   </div>
                   <div className="col-md-3 mb-3">
                     <label htmlFor="codCard">Código de segurança</label>
-                    <input type="text" className="form-control" id="codCard"  />                    
+                    <input type="text" className="form-control" id="codCard" defaultValue="737" />                    
                   </div>
                 </div>
 
@@ -202,23 +202,23 @@ const NewComponent = () =>(
                 <div className="row">
                   <div className="col-md-6 mb-3">
                     <label htmlFor="name">Nome</label>
-                    <input type="text" className="form-control" id="name"  />                    
+                    <input type="text" className="form-control" id="name" defaultValue="Bruno Samuel Raimundo Pires" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="birth">Data de nascimento</label>
-                    <input type="text" className="form-control" id="birth"  />                    
+                    <input type="text" className="form-control" id="birth" defaultValue="03/11/1984" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="cpf">CPF</label>
-                    <input type="text" className="form-control" id="cpf"  />                    
+                    <input type="text" className="form-control" id="cpf" defaultValue="024.692.438-67" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="cnpj">CNPJ</label>
-                    <input type="text" className="form-control" id="cnpj"  />                    
+                    <input type="text" className="form-control" id="cnpj" defaultValue="03.884.716/0001-97" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="passport">Passaporte</label>
-                    <input type="text" className="form-control" id="passport"  />                    
+                    <input type="text" className="form-control" id="passport" defaultValue="fghj5678" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                   <label htmlFor="email">Email</label>
@@ -226,20 +226,20 @@ const NewComponent = () =>(
                       <div className="input-group-prepend">
                         <span className="input-group-text">@</span>
                       </div>
-                      <input type="text" className="form-control" id="email"  />                    
+                      <input type="text" className="form-control" id="email" defaultValue="luizviana@publiconsult.com.rm" />                    
                     </div>
                   </div> 
                   <div className="col-md-6 mb-3">
                     <label htmlFor="pass">Senha</label>
-                    <input type="text" className="form-control" id="pass"  />                    
+                    <input type="text" className="form-control" id="pass" defaultValue="123456" />                    
                   </div> 
                   <div className="col-md-2 mb-3">
                     <label htmlFor="ddd">DDD</label>
-                    <input type="text" className="form-control" id="ddd"  />                    
+                    <input type="text" className="form-control" id="ddd" defaultValue="85" />                    
                   </div> 
                   <div className="col-md-4 mb-3">
                     <label htmlFor="phoneNumber">Telefone</label>
-                    <input type="text" className="form-control" id="phoneNumber"  />                    
+                    <input type="text" className="form-control" id="phoneNumber" defaultValue="997932664" />                    
                   </div>                   
                 </div>
 
@@ -248,31 +248,31 @@ const NewComponent = () =>(
                 <div className="row">
                   <div className="col-md-6 mb-3">
                     <label htmlFor="address">Endereço</label>
-                    <input type="text" className="form-control" id="address"  />                    
+                    <input type="text" className="form-control" id="address" defaultValue="Rua Silva Paulet" />                    
                   </div>
                   <div className="col-md-2 mb-3">
                     <label htmlFor="number">Número</label>
-                    <input type="text" className="form-control" id="number"  />                    
+                    <input type="text" className="form-control" id="number" defaultValue="665" />                    
                   </div>
                   <div className="col-md-4 mb-3">
                     <label htmlFor="zip">CEP</label>
-                    <input type="text" className="form-control" id="zip"  />                    
+                    <input type="text" className="form-control" id="zip" defaultValue="60120020" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="complement">Complemento</label>
-                    <input type="text" className="form-control" id="complement"  />                    
+                    <input type="text" className="form-control" id="complement" defaultValue="ap 802F" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="neigh">Bairro</label>
-                    <input type="text" className="form-control" id="neigh"  />                    
+                    <input type="text" className="form-control" id="neigh" defaultValue="Meireles" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="city">Cidade</label>
-                    <input type="text" className="form-control" id="city"  />                    
+                    <input type="text" className="form-control" id="city" defaultValue="Fortaleza" />                    
                   </div>
                   <div className="col-md-6 mb-3">
                     <label htmlFor="country">País</label>
-                    <input type="text" className="form-control" id="country"  />                    
+                    <input type="text" className="form-control" id="country" defaultValue="Brasil" />                    
                   </div>
                 </div>
               </form>
