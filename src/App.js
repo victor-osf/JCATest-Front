e44e9a964359ba58065c42753bc935df39b7962b
Victor Rocha <victor.rocha@ifactory.com.br>
2019-07-12 14:46:22 -0300

    adding loader old style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import './helpers/axios-interceptor'
 import HomePage from './components/HomePage';
 import ReportPage from './components/ReportPage';
 import { BrowserRouter, Route } from 'react-router-dom';
